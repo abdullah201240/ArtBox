340f923f76c2a3e746b31f7a0c510cb35a137c49
abdullah201240 <abdullahalsakib7075@gmail.com>
2024-05-30 02:47:54 +0600

    fix home page

--- Artbox/src/components/home.tsx
+++ Artbox/src/components/home.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity,Image } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image, TextStyle } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
+type GradientTextProps = {
+  text: string;
+  style?: TextStyle;
+};
+const GradientText: React.FC<GradientTextProps> = ({ text, style }) => (
+  <MaskedView
+      maskElement={
+          <Text style={[style, { backgroundColor: 'transparent' }]}>{text}</Text>
+      }
+  >
+      <LinearGradient
+          colors={['#608cfb', 'white']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+      >
+          <Text style={[style, { opacity: 0 }]}>{text}</Text>
+      </LinearGradient>
+  </MaskedView>
+);
 const Home = () => {
   return (
     <View style={styles.container}>
+      <LinearGradient
+                colors={['black', '#302b63', '#302b63', 'black', 'black', 'black']}
+                style={StyleSheet.absoluteFill}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+            />
       <View style={styles.header}>
-      <Image source={require('../../assets/logo.png')} style={styles.logo}/>        
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
         <Text style={styles.headerText}>ArtBox</Text>
+        <TouchableOpacity style={styles.upgradeButton}>
+          <Text style={styles.upgradeButtonText}>Upgrade</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.promptContainer}>
         <TextInput
@@ -59,14 +88,16 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
+    justifyContent: 'space-between', 
     marginBottom: 20,
-    marginTop:'20%'
+    marginTop: '20%',
   },
+
   headerText: {
     color: '#fff',
     fontSize: 20,
+    marginLeft: 10,  
   },
   promptContainer: {
     backgroundColor: '#111',
@@ -122,10 +153,22 @@ const styles = StyleSheet.create({
     color: '#fff',
     marginTop: 5,
   },
-  logo:{
-    width: '100%', 
-height: '100%',
-}
+  logo: {
+    width: 40,   
+    height: 40,  
+    resizeMode: 'contain', 
+  },
+  upgradeButton: {
+    borderWidth: 1,
+    borderColor: '#608cfb',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  upgradeButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
 export default Home;
